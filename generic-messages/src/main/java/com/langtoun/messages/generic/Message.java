620545c9289d6dd52b93409062847bc61eec05cc
Bruce Gilmour <bruce.gilmour@me.com>
2020-08-24 14:20:08 +0100

    Add @JsonDeserialize annotation

--- generic-messages/src/main/java/com/langtoun/messages/generic/Message.java
+++ generic-messages/src/main/java/com/langtoun/messages/generic/Message.java
@@ -1,6 +1,7 @@
 package com.langtoun.messages.generic;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.langtoun.messages.types.SerializablePayload;
 
@@ -10,6 +11,7 @@ import com.langtoun.messages.types.SerializablePayload;
  * @param <T> the generic type representing the payload
  */
 @JsonSerialize(using = MessageJsonSerializer.class)
+@JsonDeserialize(using = MessageJsonDeserializer.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Message<T extends SerializablePayload> {
 
@@ -19,14 +21,14 @@ public class Message<T extends SerializablePayload> {
     // create using factory method
   }
 
-  public T getPayload() {
-    return payload;
-  }
-
   public static <T extends SerializablePayload> Message<T> newMessage(final T payload) {
     final Message<T> message = new Message<>();
     message.payload = payload;
     return message;
+  }
+
+  public T getPayload() {
+    return payload;
   }
 
 }
